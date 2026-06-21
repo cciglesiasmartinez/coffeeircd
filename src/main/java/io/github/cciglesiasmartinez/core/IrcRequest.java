523a2f4d77cbd46150fd033191e8d738c3c3f5ee
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2026-06-21 19:30:31 +0200

    Adds JavaDoc comments.

--- src/main/java/io/github/cciglesiasmartinez/core/IrcRequest.java
+++ src/main/java/io/github/cciglesiasmartinez/core/IrcRequest.java
@@ -9,5 +9,14 @@ import java.nio.channels.SocketChannel;
 /**
  * This class represents an inbound TCP request meant to be placed in a {@link java.util.concurrent.BlockingDeque}
  * pending to be processed by any of the threads available.
+ *
+ * This class is thought to have two fields, be:
+ * <ul>
+ *     <li><b>client</b> The instance from {@link SocketChannel}.</li>
+ *     <li><b>rawCommand</b> An {@link String} object containing the raw line.</li>
+ * </ul>
+ * <p>
+ *     Objects from this class are though to be parsed into a fully parsed an ready to dispatch {@link IrcCommand}.
+ * </p>
  */
 record IrcRequest(SocketChannel client, String rawCommand) {}
